1d223dea7d44c8c89f7c90a73bed63181667c68e
Yohann-Cochet <y.yohanncochet@gmail.com>
2020-05-01 16:27:06 +0200

    page finale de présentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,41 @@
 console.log('Bienvenue sur mon profil');
 
+
+
+
+
+// MENU
+const menu1 = document.querySelector('.menu-item1');
+const menu2 = document.querySelector('.menu-item2');
+const menu3 = document.querySelector('.menu-item3');
+
+window.addEventListener('scroll', () => {
+    // j'ajoute la classe active au menu1, la retire au autres
+    if(window.scrollY < 800) {
+        menu1.classList.add('active');
+        menu2.classList.remove('active');
+        menu3.classList.remove('active');
+    }
+    // j'ajoute la classe active au menu2, la retire au autres
+    else if(window.scrollY >= 800 && window.scrollY < 1700) {
+        menu1.classList.remove('active');
+        menu2.classList.add('active');
+        menu3.classList.remove('active');
+    }
+    // j'ajoute la classe active au menu3, la retire au autres
+    else {
+        menu1.classList.remove('active');
+        menu2.classList.remove('active');
+        menu3.classList.add('active');
+    }
+    
+});
+
+
+
+
+
+// ACCORDEON
 let accordeons = document.getElementsByClassName("accordeon");
 
 for (let i = 0, l = accordeons.length; i < l; i++) {
@@ -17,18 +53,3 @@ for (let i = 0, l = accordeons.length; i < l; i++) {
         }
     }
 }
-
-// HORIZONTAL SCROLLING
-/*$(function() {
-    $("body").mousewheel(function(evt, chg) {
-       this.scrollLeft -= (chg * 50); //need a value to speed up the change
-       evt.preventDefault();
-    });
- });*/
-
-$(function() {
-    $("html, body, *").mousewheel(function(event, delta) {
-        this.scrollLeft -= (delta * 50);
-        event.preventDefault();
-    });
-});
